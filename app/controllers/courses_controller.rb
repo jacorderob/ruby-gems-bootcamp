144eba095780ae56473ca632e510dca621f17a03
Andrés Cordero <jrcorder@uc.cl>
2021-04-30 16:31:20 -0400

    Add friendly id

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -12,6 +12,7 @@ class CoursesController < ApplicationController
 
   # GET /courses/1 or /courses/1.json
   def show
+
   end
 
   # GET /courses/new
@@ -64,7 +65,7 @@ class CoursesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_course
-      @course = Course.find(params[:id])
+      @course = Course.friendly.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,7 @@
 class Course < ApplicationRecord
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
   belongs_to :user
 
   has_rich_text :description
