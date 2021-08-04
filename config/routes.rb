5e6b78fb2961a8b3b2ff2d0515b59164e6b68e65
Andrés Cordero <jrcorder@uc.cl>
2021-08-03 20:30:44 -0400

    Feature: edit user's roles

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :courses
-  resources :users, only: %i[index]
+  resources :users, only: %i[index edit show update]
   get 'home/activity'
   root 'home#index'
 end
